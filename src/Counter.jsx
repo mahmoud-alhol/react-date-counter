7d4462b6b4b7c572715391c97b3049e3c95471ab
mahmoud-alhol <mahmoud.asem.w@gmail.com>
2024-08-10 22:53:12 +0300

    v1.1

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useState } from "react";
 
 function Counter() {
-  const [step,setStep]=useState(1)
-  const [count,setCount]=useState(0)
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
 
   const date = new Date();
   date.setDate(date.getDate() + count);
@@ -11,17 +11,24 @@ function Counter() {
   return (
     <>
       <div>
-        <button onClick={()=> setStep(c=>c-1)}>-</button>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
         <span>Steps: {step}</span>
-        <button onClick={()=> setStep(c=>c+1)}>+</button>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
       </div>
       <div>
-        <button onClick={()=> setCount(c=>c-step)}>-</button>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
         <span>Count: {count}</span>
-        <button onClick={()=> setCount(c=>c+step)}>+</button>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <div>
-       <p> {date.toDateString()}</p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from now it's going to be `
+            : `${count} days ago it was `}
+          {date.toDateString()}
+        </span>
       </div>
     </>
   );
